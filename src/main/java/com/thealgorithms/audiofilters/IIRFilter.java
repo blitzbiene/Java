25a866623098f1903a1e230ad9d38f17aab0f343
blitzbiene <enigmamuzammil@gmail.com>
2023-08-14 03:00:31 +0530

    test push

--- src/main/java/com/thealgorithms/audiofilters/IIRFilter.java
+++ src/main/java/com/thealgorithms/audiofilters/IIRFilter.java
@@ -9,6 +9,7 @@ package com.thealgorithms.audiofilters;
 public class IIRFilter {
 
     private final int order;
+
     private final double[] coeffsA;
     private final double[] coeffsB;
     private final double[] historyX;
